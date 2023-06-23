7e0054993f5c257cf8200c7f8adaece4514d9b7e
wego11ya <wego11ya@gmail.com>
2023-06-23 12:31:08 +0800

    feat: R02, dashboard

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -17,17 +17,29 @@ const restaurantController = {
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
-      include: Category, // 拿出關聯的 Category model
+      include: Category
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        restaurant.increment('viewCount')
+        res.render('restaurant', {
+          restaurant: restaurant.toJSON()
+        })
+      })
+      .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: Category,
       nest: true,
       raw: true
     })
       .then(restaurant => {
-        if (!restaurant) throw new Error("Restaurant didn't exist!")
-        res.render('restaurant', {
+        if (!restaurant) throw new Error("Restaurant doesn't exist")
+        res.render('dashboard', {
           restaurant
         })
       })
-      .catch(err => next(err))
   }
 }
 module.exports = restaurantController
