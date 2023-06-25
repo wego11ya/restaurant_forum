8ae8289eba6935ed04205809739bfb4b9eefcb00
wego11ya <wego11ya@gmail.com>
2023-06-25 11:01:54 +0800

    feat: R03, user profile

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs')
 const db = require('../models')
 const { User } = db
+const { localFileHandler } = require('../helpers/file-helpers')
 const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
@@ -37,6 +38,50 @@ const userController = {
     req.flash('success_messages', '登出成功！')
     req.logout()
     res.redirect('/signin')
+  },
+  getUser: (req, res, next) => {
+    User.findByPk(req.user.id, {
+      raw: true,
+      nest: true
+    })
+      .then(user => {
+        if (!user) throw new Error('User doesn\'t exist')
+        res.render('users/profile', { user })
+      })
+      .catch(err => next(err))
+  },
+  editUser: (req, res, next) => {
+    User.findByPk(req.user.id, {
+      raw: true,
+      nest: true
+    })
+      .then(user => {
+        if (!user) throw new Error('User doesn\'t exist')
+        res.render('users/edit', { user })
+      })
+      .catch(err => next(err))
+  },
+  putUser: (req, res, next) => {
+    const { file } = req
+
+    return Promise.all([
+      User.findByPk(req.user.id),
+      localFileHandler(file)
+    ])
+      .then(([user, filePath]) => {
+        if (!user) throw new Error("User doesn't exist!")
+
+        return user.update({
+          name: req.body.name,
+          image: filePath || user.image
+        })
+      })
+      .then(() => {
+        req.flash('success_messages', '使用者資料編輯成功')
+        res.redirect(`/users/${req.params.id}`)
+      })
+      .catch(err => next(err))
   }
+
 }
 module.exports = userController
